3d34f42e288b85f8847593f7d196555f6ee7300a
dkerr0707 <dkerr0707@gmail.com>
2017-08-22 18:23:29 -0700

    Initial Commit

---
+++ calclum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  calclum
+//
+//  Created by David Kerr on 2017-08-22.
+//  Copyright © 2017 David Kerr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
